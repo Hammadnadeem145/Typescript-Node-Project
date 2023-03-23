83d56278795f9f212caa77176de9ba4dc606162b
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-24 01:04:48 +0500

    second-commit

---
+++ project01_number_guessing-game/app.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNum = Math.floor(Math.random() * 10);
+console.log(chalk.bgGreen("Number Guessing Game"));
+async function guessGame() {
+    const guess = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Enter your guess from 1 to 10",
+        },
+    ]);
+    const { userGuess } = guess;
+    console.log("Your Guess:", userGuess, "Guess:", randomNum);
+    if (userGuess == randomNum) {
+        console.log("Hurry!! Your Guess is Correct");
+    }
+    else {
+        console.log("Please try again");
+    }
+}
+guessGame();
--- project01_number_guessing-game/app.ts
+++ project01_number_guessing-game/app.ts
@@ -3,3 +3,24 @@ import chalk from "chalk";
 
 const randomNum = Math.floor(Math.random() * 10);
 
+console.log(chalk.bgGreen("Number Guessing Game"));
+
+async function guessGame() {
+  const guess: { userGuess: number } = await inquirer.prompt([
+    {
+      type: "number",
+      name: "userGuess",
+      message: "Enter your guess from 1 to 10",
+    },
+  ]);
+
+  const { userGuess } = guess;
+
+  console.log("Your Guess:", userGuess, "Guess:", randomNum,);
+  if (userGuess == randomNum) {
+    console.log("Hurry!! Your Guess is Correct");
+  } else {
+    console.log("Please try again");
+  }
+}
+guessGame();
