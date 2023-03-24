47f0f8c1b93141e583402fa9bc025b49c8d9db80
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-25 01:31:45 +0500

    student-management

--- project06_student_management_system/app.js
+++ project06_student_management_system/app.js
@@ -1,94 +1,74 @@
-
-
 class Person {
-  name: string;
-  age: number;
-  
-  constructor(name: string, age: number, ) {
-    this.name = name;
-    this.age = age;
-    
-  }
-  getName() {
-    return this.name;
-  }
-}
-
-class Student extends Person {
-rollNumber: String;
-courses: Course[] = []
-constructor(name: string, age: number, rollNumber: string) {
-    super(name, age);
-    this.rollNumber = rollNumber;
-}
-registerCourse(course: Course) {
-    this.courses.push(course);
-}
-}
-
-class Instructor extends Person {
-    private salary: number;
-    courses: Course[] = [];
-    constructor(name:string, age: number, salary: number) {
-        super(name, age)
-        this.salary = salary;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    assignCourse(course: Course) {
+    getName() {
+        return this.name;
+    }
+}
+class Student extends Person {
+    constructor(name, age, rollNumber) {
+        super(name, age);
+        this.courses = [];
+        this.rollNumber = rollNumber;
+    }
+    registerCourse(course) {
         this.courses.push(course);
     }
 }
-
+class Instructor extends Person {
+    constructor(name, age, salary) {
+        super(name, age);
+        this.courses = [];
+        this.salary = salary;
+    }
+    assignCourse(course) {
+        this.courses.push(course);
+    }
+}
 class Course {
-    id: string;
-    name: string;
-    students: Student[] = [];
-    instructor!: Instructor;
-    constructor(id: string, name: string) {
+    constructor(id, name) {
+        this.students = [];
         this.id = id;
         this.name = name;
     }
-    addStudent(student: Student) {
+    addStudent(student) {
         this.students.push(student);
         student.registerCourse(this);
     }
-    setInstructor(instructor: Instructor) {
+    setInstructor(instructor) {
         this.instructor = instructor;
-        instructor.assignCourse(this)
+        instructor.assignCourse(this);
     }
 }
-
 class Department {
-    name: string;
-    courses: Course[] = [];
-    constructor(name: string) {
+    constructor(name) {
+        this.courses = [];
         this.name = name;
     }
-    addCourse(course: Course) {
+    addCourse(course) {
         this.courses.push(course);
     }
 }
-
 const student1 = new Student("Ruwaifa", 21, "03144252044");
 const student2 = new Student("Salman", 21, "03344529308");
 const instructor1 = new Instructor("Zia Khan", 25, 45000);
 const instructor2 = new Instructor("Zeeshan Hanif", 36, 45000);
 const course1 = new Course("3.0", "Metaverse and Blockchin");
 const course2 = new Course("5.0", "AI");
-
-
 course1.addStudent(student1);
 course1.addStudent(student2);
 course1.setInstructor(instructor1);
 course1.setInstructor(instructor2);
 // console.log(course1);
-
 const department = new Department("Web Programming");
 department.addCourse(course1);
 department.addCourse(course2);
-
 console.log('Student1:', student1);
 console.log('Student2:', student2);
 console.log('Instructor1:', instructor1);
 console.log('Instructor2:', instructor2);
 console.log('course1:', course1);
 console.log('course2:', course2);
+export {};
