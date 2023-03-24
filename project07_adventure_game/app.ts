0176c3e9fbcc76e11e211fdd00090b6a05fa5ae8
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-25 02:04:04 +0500

    adventureGame-done

---
+++ project07_adventure_game/app.ts
@@ -0,0 +1,54 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+interface Adventure {
+    name: string;
+    location: string;
+}
+
+const adventure: Adventure = {
+    name: "Hide and Seek",
+    location: "A scary forest, Haunted House, Cave",
+};
+
+console.log(chalk.rgb(123, 45, 67).underline("WELCOME TO ADVENTURE GAME!"));
+
+const gameStart = async function () {
+    console.log(` You find yourself in ${adventure.location}.`);
+
+
+const adventureName = await inquirer.prompt([{
+    type: "list",
+    name: "name",
+    message: "Where would you like to do play?",
+      choices: [
+        "Scary forest",
+        "Haunted House",
+        "Cave",
+        "Go back home",
+        "Quit the game",
+      ],
+},
+]);
+
+switch (adventureName.name) {
+    case "Scary forest":
+        console.log("You start exploring the forest.");
+        break;
+  
+      case "Haunted House":
+        console.log("You start exploring the haunted house.");
+        break;
+      case "Cave":
+        console.log("You start exploring the Cave.");
+        break;
+      case "Go back home":
+        console.log("You go back home.");
+        break;
+      case "Quit the game":
+        console.log("Thanks for playing!");
+        process.exit(0);
+    }
+};
+
+gameStart();
