61169162cf2d36afc9ce7429e8eb7cd216fec6df
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-25 02:52:15 +0500

    countdown-finish

---
+++ project09_countdown_timer/app.js
@@ -0,0 +1,28 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.bgBlueBright('COUNTDOWN TIMER'));
+inquirer.prompt([
+    {
+        type: "input",
+        name: "duration",
+        message: "Enter the duration for the coutdown in seconds",
+        validate: (input) => {
+            const value = parseInt(input);
+            if (!value || value <= 0) {
+                return "Please enter duration greather than 0";
+            }
+            return true;
+        },
+    },
+]).then((answer) => {
+    const duration = parseInt(answer.duration);
+    let count = duration;
+    const interval = setInterval(() => {
+        console.log(count);
+        count--;
+        if (count < 0) {
+            clearInterval(interval);
+            console.log("Countdown finished");
+        }
+    }, 1000);
+});
